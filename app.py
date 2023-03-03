a82e4e89712fca90493f5499b94d31f469d8a3b7
ashishraut64 <ashishraut64@gmail.com>
2023-03-03 11:57:44 +0530

    Removed numpy module

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 st.set_page_config(layout='wide',page_title='Startup Analysis')
 df = pd.read_csv('startup_cleaned.csv')
